e58e3cf7d752e851b9d0b5864499df8643d87de7
MrJiawenTang <1074356353@qq.com>
2022-09-08 16:30:57 +0800

    fix bug

--- cqc-web-api/src/main/java/com/cloud/cqc/client/common/constant/AppConst.java
+++ cqc-web-api/src/main/java/com/cloud/cqc/client/common/constant/AppConst.java
@@ -3,6 +3,6 @@ package com.cloud.cqc.client.common.constant;
 public interface AppConst {
 
 	/** 忽略校验 */
-	static final String[] SECURITY_INGORE_URLS = { "/upload/**", "/api/**" };
+	static final String[] SECURITY_INGORE_URLS = { "/upload/**", "/api/**" ,"/certificate/**"};
 
 }
